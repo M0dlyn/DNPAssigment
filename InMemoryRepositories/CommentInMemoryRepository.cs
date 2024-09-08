2f168b36d1be7ddf8f68e5fca0127e4d98f8c6eb
Mario-Adrian Vlad <344345@via.dk>
2024-09-08 21:40:43 +0200

    implemented user and comment InMemoryRepository

---
+++ InMemoryRepositories/CommentInMemoryRepository.cs
@@ -0,0 +1,59 @@
+﻿using System.Formats.Tar;
+using Entities;
+using RepositoryContracts;
+
+namespace InMemoryRepositories;
+
+public class CommentInMemoryRepository : ICommentRepository
+{
+    
+    List<Comment> comments = new List<Comment>();
+
+
+    public Task<Comment> AddAsync(Comment comment)
+    {
+        comment.Id = comments.Any();
+            ? comments.Max(comment => comment.Id) + 1
+            : 1;
+        comments.Add(comment);
+        return Task.FromResult(comment);
+    }
+
+    public Task UpdateAsync(Comment comment)
+    {
+        Comment? existingComment = comments.SingleOrDefault(c => c.Id == comment.Id);
+        if (existingComment == null)
+        {
+            throw new InvalidOperationException($"Comment with Id '{comment.Id}' does not exist");
+        }
+        comments.Remove(existingComment);
+        comments.Add(comment);
+        return Task.CompletedTask;
+    }
+
+    public Task DeleteAsync(int id)
+    {
+        Comment? commentToRemove = comments.SingleOrDefault(c => c.Id == id);
+        if (commentToRemove is null)
+        {
+            throw new InvalidOperationException($"Comment with Id '{id}' does not exist");
+        }
+        comments.Remove(commentToRemove);
+        return Task.CompletedTask;
+    }
+
+    public Task<Comment> GetSingleAsync(int id)
+    {
+        Comment? comment = comments.SingleOrDefault(c => c.Id == id);
+        if (comment is null)
+        {
+            throw new InvalidOperationException($"Comment with Id '{id}' does not exist");
+        }
+        return Task.FromResult(comment);
+    }
+
+    public IQueryable<Comment> GetMany()
+    {
+        return comments.AsQueryable();
+    }
+}
---
+++ InMemoryRepositories/UserInMemoryRepository.cs
@@ -0,0 +1,64 @@
+﻿using System.Formats.Tar;
+using Entities;
+using RepositoryContracts;
+
+
+namespace InMemoryRepositories;
+
+public class UserInMemoryRepository : IUserRepository
+{
+    private List<User> users = new List<User>();
+
+    public Task<User> AddAsync(User user)
+    {
+
+        user.Id = users.Any();
+            ? users.Max(user  => user.Id) + 1
+            : 1;
+        users.Add(user);
+        return Task.FromResult(user);
+        
+    }
+
+    public Task UpdateAsync(User user)
+    {
+        
+        User? existingUser = users.SingleOrDefault(u => u.Id == user.Id);
+        if (existingUser == null)
+        {
+            throw new InvalidOperationException($"User with Id '{user.Id}' does not exist");
+        }
+        users.Remove(existingUser);
+        users.Add(user);
+       return Task.CompletedTask;
+    }
+
+    public Task DeleteAsync(int id)
+    {
+        User? userToRemove = users.SingleOrDefault(u => u.Id == id);
+        if (userToRemove is null)
+        {
+            throw new InvalidOperationException($"User with Id '{id}' does not exist");
+        }
+        
+        users.Remove(userToRemove);
+        return Task.CompletedTask;
+    }
+
+    public Task<User> GetSingleAsync(int Id)
+    {
+        User? user = users.SingleOrDefault(u => u.Id == Id);
+
+        if (user is null)
+        {
+            throw new InvalidOperationException($"User with Id '{Id}' does not exist");
+        }
+        
+        return Task.FromResult(user);
+    }
+
+    public IQueryable<User> GetMany()
+    {
+        return users.AsQueryable();
+    }
+}
