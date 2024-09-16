ffeaaf85838c0d92c58d9b4b07aa7c2eea905570
Mario-Adrian Vlad <344345@via.dk>
2024-09-16 09:54:07 +0200

    crated CLI app, UI and ManagePosts directories

---
+++ Server/CLI/UI/CliApp.cs
@@ -0,0 +1,24 @@
+﻿namespace CLI.UI;
+
+using InMemoryRepositories;
+using RepositoryContracts;
+
+
+public class CliApp
+{
+
+    public CliApp()
+    {
+        
+    }
+
+    public CliApp(IUserRepository userRepository, ICommentRepository commentRepository, IPostRepository postRepository)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task StartAsync()
+    {
+        throw new NotImplementedException();
+    }
+}
