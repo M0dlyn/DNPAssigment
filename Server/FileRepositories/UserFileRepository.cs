8ac0aea9cc26cea951a3e28480f996aec9108aa6
Mario-Adrian Vlad <344345@via.dk>
2024-09-23 10:52:00 +0200

    Added user and post file repositories

---
+++ Server/FileRepositories/UserFileRepository.cs
@@ -0,0 +1,78 @@
+﻿using System.Text.Json;
+using Entities;
+using RepositoryContracts;
+
+namespace FileRepositories;
+
+public class UserFileRepository : IUserRepository
+{
+    
+    private readonly string filePath = "users.json";
+    
+    public UserFileRepository()
+    {
+        if (!File.Exists(filePath))
+        {
+            File.WriteAllText(filePath, "[]");
+        }
+    }
+    public async Task<User> AddAsync(User user)
+    {
+        string usersAsJson = await File.ReadAllTextAsync(filePath);
+        List<User> users = JsonSerializer.Deserialize<List<User>>(usersAsJson);
+        int maxId = users.Count > 0 ? users.Max(u => u.Id) : 1;
+        user.Id = maxId + 1;
+        users.Add(user);
+        usersAsJson = JsonSerializer.Serialize(users);
+        await File.WriteAllTextAsync(filePath, usersAsJson);
+        return user;
+    }
+
+    public async Task UpdateAsync(User user)
+    {
+        string usersAsJson = await File.ReadAllTextAsync(filePath);
+        List<User> users = JsonSerializer.Deserialize<List<User>>(usersAsJson);
+        User? existingUser = users.SingleOrDefault(u => u.Id == user.Id);
+        if (existingUser == null)
+        {
+            throw new InvalidOperationException($"User with Id ' {user.Id}' does not exist");
+        }
+        users.Remove(existingUser);
+        users.Add(user);
+        usersAsJson = JsonSerializer.Serialize(users);
+        await File.WriteAllTextAsync(filePath, usersAsJson);
+    }
+
+    public async Task DeleteAsync(int id)
+    {
+        string usersAsJson = await File.ReadAllTextAsync(filePath);
+        List<User> users = JsonSerializer.Deserialize<List<User>>(usersAsJson);
+        User? userToRemove = users.SingleOrDefault(u => u.Id == id);
+        if (userToRemove is null)
+        {
+            throw new InvalidOperationException($"User with Id ' {id}' does not exist");
+        }
+        users.Remove(userToRemove);
+        usersAsJson = JsonSerializer.Serialize(users);
+        await File.WriteAllTextAsync(filePath, usersAsJson);
+    }
+
+    public async Task<User> GetSingleAsync(int Id)
+    {
+        string usersAsJson = await File.ReadAllTextAsync(filePath);
+        List<User> users = JsonSerializer.Deserialize<List<User>>(usersAsJson);
+        User? user = users.SingleOrDefault(u => u.Id == Id);
+        if (user is null)
+        {
+            throw new InvalidOperationException($"User with Id ' {Id}' does not exist");
+        }
+        return user;
+    }
+
+    public IQueryable<User> GetMany()
+    {
+        string usersAsJson = File.ReadAllText(filePath);
+        List<User> users = JsonSerializer.Deserialize<List<User>>(usersAsJson);
+        return users.AsQueryable();
+    }
+}
