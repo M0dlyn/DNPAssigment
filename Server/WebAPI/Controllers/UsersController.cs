6a2618a94d968d38c96cd5eff1c34cd05650a634
Mario-Adrian Vlad <344345@via.dk>
2024-10-21 17:31:50 +0200

    Created user dtos and controller for user

---
+++ Server/WebAPI/Controllers/UsersController.cs
@@ -0,0 +1,56 @@
+﻿using ApiContracts;
+using Entities;
+using Microsoft.AspNetCore.Mvc;
+using RepositoryContracts;
+
+namespace WebAPI.Controllers;
+[ApiController]
+[Route("[controller]")]
+
+public class UsersController : ControllerBase
+{
+    
+    private readonly IUserRepository userRepo;
+    
+    public UsersController(IUserRepository userRepo)
+    {
+        this.userRepo = userRepo;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<UserDto>> CreateUser([FromBody] CreateUserDto request)
+    {
+        try
+        {
+            User user = new(request.Username, request.Password);
+            User created = await userRepo.AddAsync(user);
+            UserDto userDto = new(created.Id, created.Username);
+            return Created($"/Users/{userDto.Id}", created);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return StatusCode(500, e.Message);
+        }
+
+    }
+    
+    [HttpGet("{id}")]
+    public async Task<ActionResult<UserDto>> GetSingleUser([FromRoute]int id)
+    {
+        try
+        {
+            User result =  await userRepo.GetSingleAsync(id);
+            UserDto userDto = new(result.Id, result.Username);
+            return Ok(userDto); 
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return NotFound(e.Message);
+        }
+    }
+    
+    []
+    
+}
---
+++ Shared/ApiContracts/UserDto.cs
@@ -0,0 +1,17 @@
+﻿namespace ApiContracts;
+
+public class UserDto
+{
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public int Id { get; set; }
+    
+    public UserDto(int id, string username)
+    {
+        Username = username;
+        Id = id;
+        
+    }
+
+  
+}
