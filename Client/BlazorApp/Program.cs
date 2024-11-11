7ba9ee95b364f6307ef8024291bb5e9dcdb84c27
Mario-Adrian Vlad <344345@via.dk>
2024-11-11 16:19:43 +0100

    started to create the Create post page

--- Client/BlazorApp/Program.cs
+++ Client/BlazorApp/Program.cs
@@ -1,4 +1,5 @@
 using BlazorApp.Components;
+using BlazorApp.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,7 +8,13 @@ builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:5001") });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:5150") });
+
+builder.Services.AddScoped<IPostService, HttpPostService>();
+builder.Services.AddScoped<IUserService, HttpUserService>();
+builder.Services.AddScoped<ICommentService, HttpCommentService>();
+
+
 
 var app = builder.Build();
 
