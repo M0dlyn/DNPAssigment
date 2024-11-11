056d4d92395b75846918cb1ef4f1d01e21f5b2a4
Mario-Adrian Vlad <344345@via.dk>
2024-11-11 14:28:37 +0100

    Created Service interfaces and classes for user, comments and posts

---
+++ Client/BlazorApp/Services/HttpUserService.cs
@@ -0,0 +1,39 @@
+﻿using ApiContracts;
+
+namespace BlazorApp.Services;
+
+public class HttpUserService : IUserService
+{
+
+    private readonly HttpClient client;
+    
+    public HttpUserService(HttpClient client)
+    {
+        this.client = client;
+    }
+    
+    public Task<UserDto> AddUserAsync(CreateUserDto request)
+    {
+     
+    }
+
+    public Task UpdateUserAsync(UpdateUserDto request)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteUserAsync(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<UserDto> GetUserAsync(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<UserDto>> GetUsersAsync()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Client/BlazorApp/Services/IUserService.cs
@@ -0,0 +1,12 @@
+﻿using ApiContracts;
+
+namespace BlazorApp.Services;
+
+public interface IUserService
+{
+    public Task<UserDto> AddUserAsync(CreateUserDto request);
+    public Task UpdateUserAsync(UpdateUserDto request);
+    public Task DeleteUserAsync(Guid id);
+    public Task<UserDto> GetUserAsync(Guid id);
+    public Task<List<UserDto>> GetUsersAsync();
+}
